6f7f71bd78f9d52b590ed783934431bb81f12243
Tauheed Butt <tauheedbutt@gmail.com>
2023-03-12 08:56:36 +0500

    Rearrange Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { data } from './config/constants';
+
+import SearchBar from './components/SearchBar';
 import Table from './components/Table'
 
 const App = () => {
+  const [search, setSearch] = useState('');
+  const [stock, setStock] = useState(false);
+  const labels = ['Name', 'Price'];
+
+  const filtered = data?.filter((item) => (
+    item.name.toLowerCase().includes(search.toLowerCase()) && (stock ? item.stocked : true)
+  ));
+
   return (
     <div className='container pt-5 pb-5'>
-      <Table />
+      <SearchBar search={search} setSearch={setSearch} stock={stock} setStock={setStock} />
+      <Table labels={labels} data={filtered} />
     </div>
   )
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,11 @@
-import React, { useState } from 'react'
-import { data } from '../config/constants';
+import React from 'react'
 import Labels from './Labels';
 import Products from './Products';
 
-import SearchBar from './SearchBar';
-
-const Table = () => {
-    const [search, setSearch] = useState('');
-    const [stock, setStock] = useState(false);
-    const labels = ['Name', 'Price'];
-
-    const filtered = data?.filter((item) => (
-        item.name.toLowerCase().includes(search.toLowerCase()) && (stock ? item.stocked : true)
-    ));
-
-    const categories = Array.from(new Set(filtered.map(item => item.category)));
-    console.log(search.toLowerCase())
-
+const Table = ({ labels, data }) => {
+    const categories = Array.from(new Set(data.map(item => item.category)));
     return (
         <div>
-            <SearchBar
-                search={search}
-                setSearch={setSearch}
-                stock={stock}
-                setStock={setStock}
-            />
             <div className='d-flex flex-column rounded shadow bg-white p-2 mt-5'>
                 <Labels labels={labels} />
                 {
@@ -32,7 +13,7 @@ const Table = () => {
                         <Products
                             key={index}
                             title={category}
-                            data={filtered?.filter(item => item.category == category)}
+                            data={data?.filter(item => item.category == category)}
                         />
                     ))
                 }
