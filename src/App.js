06f47014c3f97ff5e661293489e7a45961d07f57
Tauheed Butt <tauheedbutt@gmail.com>
2023-03-12 08:57:59 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import SearchBar from './components/SearchBar';
 import Table from './components/Table'
 
 const App = () => {
+  // States
   const [search, setSearch] = useState('');
   const [stock, setStock] = useState(false);
-  const labels = ['Name', 'Price'];
 
+  // Constatns
+  const labels = ['Name', 'Price'];
   const filtered = data?.filter((item) => (
     item.name.toLowerCase().includes(search.toLowerCase()) && (stock ? item.stocked : true)
   ));
 
   return (
-    <div className='container pt-5 pb-5'>
+    <div className='container d-flex flex-column pt-5 pb-5 gap-5'>
       <SearchBar search={search} setSearch={setSearch} stock={stock} setStock={setStock} />
       <Table labels={labels} data={filtered} />
     </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,7 @@ const Table = ({ labels, data }) => {
     const categories = Array.from(new Set(data.map(item => item.category)));
     return (
         <div>
-            <div className='d-flex flex-column rounded shadow bg-white p-2 mt-5'>
+            <div className='d-flex flex-column rounded shadow bg-white p-4'>
                 <Labels labels={labels} />
                 {
                     categories?.map((category, index) => (
