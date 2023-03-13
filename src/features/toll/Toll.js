044c170d60074104a62e24e617913fe444feb3d9
Tauheed Butt <tauheedbutt@gmail.com>
2023-03-13 13:14:29 +0500

    No Cards

--- src/features/toll/Toll.js
+++ src/features/toll/Toll.js
@@ -62,9 +62,9 @@ const Toll = () => {
     return (
         <div className='p-2'>
             <Header />
-            <div className='p-3'>
-                <div className='row mt-5 d-flex justify-content-between'>
-                    <div ref={ref} className='col-12 col-lg-4 col-xl-4 p-5 mb-5 rounded bg-white shadow' style={{height: 'max-content'}}>
+            <div className='p-3 mt-5'>
+                <div className='row d-flex justify-content-between'>
+                    <div ref={ref} className='col-12 col-lg-4 col-xl-4 p-5 pt-3 mb-5 rounded bg-white' style={{height: 'max-content'}}>
                         <Form fields={fields} />
                         <SubmitButton
                             title={isUpdate ? 'Mark Exit' : 'New Entry'}
@@ -81,7 +81,7 @@ const Toll = () => {
                             />
                         }
                     </div>
-                    <div className='col-12 col-lg-7 col-xl-7 p-3 rounded shadow' style={{height: 'max-content'}}>
+                    <div className='col-12 col-lg-8 col-xl-8 p-3 rounded' style={{height: 'max-content'}}>
                         <Table onEdit={(item) => {
                             ref.current?.scrollIntoView();
                             setData({ ...item, day: new Date(item.day) })
